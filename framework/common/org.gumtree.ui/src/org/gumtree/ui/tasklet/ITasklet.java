bf549d0acf12f6553d03c2ca52942a83c0a3e08a
calltl <calltl@gmail.com>
2012-08-10 12:41:06 +1000

    UI test code

---
+++ framework/common/org.gumtree.ui/src/org/gumtree/ui/tasklet/ITasklet.java
@@ -0,0 +1,9 @@
+package org.gumtree.ui.tasklet;
+
+import java.util.List;
+
+public interface ITasklet {
+
+	public String getTags();
+	
+}
--- framework/common/org.gumtree.ui/src/org/gumtree/ui/tasklet/ITaskletManager.java
+++ framework/common/org.gumtree.ui/src/org/gumtree/ui/tasklet/ITaskletManager.java
@@ -1,5 +1,13 @@
 package org.gumtree.ui.tasklet;
 
+import java.util.List;
+
 public interface ITaskletManager {
 
+	public void addTasklet(ITasklet tasklet);
+	
+	public void removeTasklet(ITasklet tasklet);
+	
+	public List<ITasklet> getTasklets();	
+	
 }
