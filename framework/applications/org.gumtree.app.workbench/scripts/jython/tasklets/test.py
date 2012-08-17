b38280084410cbb1dc06f92ab3247002c626827c
calltl <calltl@gmail.com>
2012-08-17 13:27:06 +1000

    Added parameterised runnable for UI

--- framework/applications/org.gumtree.app.workbench/scripts/jython/tasklets/test.py
+++ framework/applications/org.gumtree.app.workbench/scripts/jython/tasklets/test.py
@@ -1,8 +1,2 @@
-from java.lang import System
-
-from org.eclipse.ui import PlatformUI
-
 def createPerspective(mPerspective):
-    System.out.println(mPerspective)
-    workbench = PlatformUI.getWorkbench()
-    workbench.openWorkbenchWindow(None)
+    pass
---
+++ framework/common/org.gumtree.ui/src/org/gumtree/ui/util/ParameterizedSafeRunnable.java
@@ -0,0 +1,30 @@
+package org.gumtree.ui.util;
+
+import org.eclipse.jface.util.SafeRunnable;
+
+public abstract class ParameterizedSafeRunnable<T> extends SafeRunnable {
+
+	private T context;
+
+	public ParameterizedSafeRunnable(T context) {
+		super();
+		this.context = context;
+	}
+
+	public ParameterizedSafeRunnable(T context, String message) {
+		super(message);
+		this.context = context;
+	}
+
+	@Override
+	public void run() throws Exception {
+		run(getContext());
+	}
+
+	public abstract void run(T context) throws Exception;
+
+	public T getContext() {
+		return context;
+	}
+
+}
