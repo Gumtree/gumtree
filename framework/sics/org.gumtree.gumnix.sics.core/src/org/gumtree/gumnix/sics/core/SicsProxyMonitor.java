9145eead04a93c06d99253f7e56b6fefcb79fa48
calltl <calltl@gmail.com>
2013-02-19 14:58:04 +1100

    Fixed NPE on missing application context service

--- framework/sics/org.gumtree.gumnix.sics.core/src/org/gumtree/gumnix/sics/core/SicsProxyMonitor.java
+++ framework/sics/org.gumtree.gumnix.sics.core/src/org/gumtree/gumnix/sics/core/SicsProxyMonitor.java
@@ -4,14 +4,11 @@ import javax.management.InstanceNotFoundException;
 import javax.management.MBeanRegistrationException;
 import javax.management.MalformedObjectNameException;
 
-import org.gumtree.core.service.ServiceUtils;
-import org.gumtree.gumnix.sics.internal.core.Activator;
 import org.gumtree.gumnix.sics.io.ISicsChannelMonitor;
 import org.gumtree.gumnix.sics.io.SicsProxyListenerAdapter;
 import org.gumtree.util.jmx.ExtendedMBeanExporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jmx.export.MBeanExportException;
 import org.springframework.jmx.export.MBeanExporter;
 import org.springframework.jmx.support.ObjectNameManager;
@@ -62,12 +59,7 @@ public class SicsProxyMonitor implements ISicsProxyMonitor {
 	}
 
 	private void unregisterConnectionMonitors() {
-		ApplicationContext context = ServiceUtils.getServiceManager()
-				.getService(ApplicationContext.class,
-						"org.springframework.context.service.name",
-						Activator.PLUGIN_ID);
-		MBeanExporter exporter = (MBeanExporter) context.getBean(
-				"sicJmxExporter", MBeanExporter.class);
+		MBeanExporter exporter = getExporter();
 		for (ISicsChannelMonitor monitor : SicsCore.getDefaultProxy()
 				.getChannelMonitors()) {
 			try {
