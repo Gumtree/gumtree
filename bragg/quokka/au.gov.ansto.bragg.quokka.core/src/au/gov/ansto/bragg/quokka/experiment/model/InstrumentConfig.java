1e50dcd944f9b6b4af19f0b29ec932eb3371129b
calltl <calltl@gmail.com>
2013-04-05 14:09:35 +1100

    Added detector distance auto detection.

--- bragg/quokka/au.gov.ansto.bragg.quokka.core/src/au/gov/ansto/bragg/quokka/experiment/model/InstrumentConfig.java
+++ bragg/quokka/au.gov.ansto.bragg.quokka.core/src/au/gov/ansto/bragg/quokka/experiment/model/InstrumentConfig.java
@@ -13,6 +13,8 @@ package au.gov.ansto.bragg.quokka.experiment.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InstrumentConfig extends AbstractModelObject {
 	
@@ -23,6 +25,8 @@ public class InstrumentConfig extends AbstractModelObject {
 		modeList.add("Counter");
 		modeList.add("Timer");
 	}
+	
+	private static Pattern pattern = Pattern.compile("driveDet\\(\\S+,");
 	
 	private String name;
 	
@@ -48,6 +52,8 @@ public class InstrumentConfig extends AbstractModelObject {
 	private List<String> availableModes;
 	
 	private InstrumentConfigTemplate template;
+	
+	private Float detectorDistance;
 	
 	// [[GT-207] The following 3 attributes relate to the file association propagation
 	private String emptyCellTransmissionDataFile;
@@ -110,6 +116,20 @@ public class InstrumentConfig extends AbstractModelObject {
 	public void setInitScript(String initScript) {
 		String oldValue = this.initScript;
 		this.initScript = initScript;
+		
+		// Calculate detector distance
+		Matcher matcher = pattern.matcher(initScript);
+	    boolean matchFound = matcher.find();
+	    // Only count on the first appears
+	    if (matchFound) {
+	    	try {
+				Float detectorDistance = Float.parseFloat(initScript.substring(
+						matcher.start() + "driveDet(".length(), matcher.end()
+								- ",".length()));
+				setDetectorDistance(detectorDistance);
+	    	} catch (Exception e) {
+	    	}
+	    }
 		firePropertyChange("initScript", oldValue, initScript);
 	}
 
@@ -193,6 +213,16 @@ public class InstrumentConfig extends AbstractModelObject {
 		setStartingAttenuation(getTemplate().getStartingAtteunation());
 	}
 
+	public Float getDetectorDistance() {
+		return detectorDistance;
+	}
+	
+	public void setDetectorDistance(Float detectorDistance) {
+		Float oldValue = this.detectorDistance;
+		this.detectorDistance = detectorDistance;
+		firePropertyChange("detectorDistance", oldValue, detectorDistance);
+	}
+	
 	public String getEmptyCellTransmissionDataFile() {
 		return emptyCellTransmissionDataFile;
 	}
