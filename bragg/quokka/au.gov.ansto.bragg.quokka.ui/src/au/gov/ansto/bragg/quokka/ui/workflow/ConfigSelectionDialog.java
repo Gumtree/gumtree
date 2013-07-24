f140291bbefdb6fe707925f4162b0570a0aec4b7
David Mannicke <davidm@ansto>
2013-07-24 13:00:45 +1000

    configuration files for the multi sample workflow can now be sorted by name and date/time

--- bragg/quokka/au.gov.ansto.bragg.quokka.ui/src/au/gov/ansto/bragg/quokka/ui/workflow/ConfigSelectionDialog.java
+++ bragg/quokka/au.gov.ansto.bragg.quokka.ui/src/au/gov/ansto/bragg/quokka/ui/workflow/ConfigSelectionDialog.java
@@ -29,9 +29,11 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -45,6 +47,13 @@ import au.gov.ansto.bragg.quokka.ui.internal.InternalImage;
 
 public class ConfigSelectionDialog extends MessageDialog {
 
+	enum ContentSorting {
+	    BY_NAME, BY_DATE
+	}
+
+	private static final String SORT_BY_NAME = "sort by name";
+	private static final String SORT_BY_DATE = "sort by date";
+	
 	private IFolder baseDirectory;
 	
 	private boolean isNewConfig = false;
@@ -52,6 +61,7 @@ public class ConfigSelectionDialog extends MessageDialog {
 	private IFile selectedConfig;
 	
 	private TreeViewer treeViewer;
+	private ConfigContentProvider contentProvider;
 	
 	public ConfigSelectionDialog(Shell parentShell) {
 		super(parentShell, "Select instrument configuration", null,
@@ -87,8 +97,7 @@ public class ConfigSelectionDialog extends MessageDialog {
 	
 	protected Control createCustomArea(Composite parent) {
 		Composite mainArea = new Composite(parent, SWT.NONE);
-		mainArea.setLayout(new GridLayout(3, false));
-		
+		mainArea.setLayout(new GridLayout(5, false));
 		
 		Button templateButton = new Button(mainArea, SWT.RADIO);
 		templateButton.setText("From an existing configuration");
@@ -100,9 +109,7 @@ public class ConfigSelectionDialog extends MessageDialog {
 			}
 		});
 
-		
-		Label label = new Label(mainArea, SWT.NONE);
-		label.setText("                  ");
+		(new Label(mainArea, SWT.NONE)).setText("                  ");
 		
 		Button newButton = new Button(mainArea, SWT.RADIO);
 		newButton.setText("New an empty configuration");
@@ -113,12 +120,20 @@ public class ConfigSelectionDialog extends MessageDialog {
 				isNewConfig = true;
 			}
 		});
+
+		(new Label(mainArea, SWT.NONE)).setText("                  ");
+		
+		Combo sortByCombo = new Combo(mainArea, SWT.DROP_DOWN | SWT.BORDER | SWT.READ_ONLY);
+		sortByCombo.add(SORT_BY_NAME);
+		sortByCombo.add(SORT_BY_DATE);
+		sortByCombo.select(0);
 		
 		treeViewer = new TreeViewer(parent, SWT.BORDER | SWT.VIRTUAL | SWT.SINGLE | SWT.FULL_SELECTION | SWT.V_SCROLL);
 		Tree tree = treeViewer.getTree();
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).hint(800, 400).span(3, 1).applyTo(tree);
 		tree.setHeaderVisible(true);
-		treeViewer.setContentProvider(new ConfigContentProvider());
+		contentProvider = new ConfigContentProvider(ContentSorting.BY_NAME);
+		treeViewer.setContentProvider(contentProvider);
 		treeViewer.setUseHashlookup(true);
 		treeViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
@@ -130,6 +145,8 @@ public class ConfigSelectionDialog extends MessageDialog {
 				}
 			}
 		});
+
+		sortByCombo.addSelectionListener(new SortSelectionListener(treeViewer, contentProvider));
 		
 		TreeViewerColumn nameColumn = new TreeViewerColumn(treeViewer, SWT.LEFT);
 		nameColumn.getColumn().setText("Name");
@@ -179,10 +196,12 @@ public class ConfigSelectionDialog extends MessageDialog {
 							StringBuilder stringBuilder = new StringBuilder();
 			
 							String line;
+							String newLine = System.getProperty("line.separator");
 							while ((line = reader.readLine()) != null) {
 								stringBuilder.append(line.replace(
 										"au.gov.ansto.bragg.quokka2.experiment.model.InstrumentConfigTemplate",
 										"au.gov.ansto.bragg.quokka.experiment.model.InstrumentConfigTemplate"));
+								stringBuilder.append(newLine);
 						    }
 							
 							Object object = ExperimentModelUtils.getXStream().fromXML(stringBuilder.toString());
@@ -201,7 +220,6 @@ public class ConfigSelectionDialog extends MessageDialog {
 		});
 		
 		treeViewer.setInput(getBaseDirectory());
-		
 		
 		return mainArea;
 	}
@@ -223,12 +241,46 @@ public class ConfigSelectionDialog extends MessageDialog {
 		}
 		display.dispose();
 	}
+	
+	class SortSelectionListener implements SelectionListener {
+
+		private TreeViewer treeViewer;
+		private ConfigContentProvider contentProvider;
+		
+		public SortSelectionListener(TreeViewer treeViewer, ConfigContentProvider contentProvider) {
+			this.treeViewer = treeViewer;
+			this.contentProvider = contentProvider;
+		}
+		
+		public void widgetSelected(SelectionEvent e) {
+			// update content provider
+			String selection = ((Combo)e.getSource()).getText();
+			if (selection.equals(SORT_BY_NAME))
+				contentProvider.setSorting(ContentSorting.BY_NAME);
+			else // if (selection.equals(SORT_BY_DATE))
+				contentProvider.setSorting(ContentSorting.BY_DATE);
+			
+			// reload content
+			treeViewer.setInput(getBaseDirectory());
+		}
+		public void widgetDefaultSelected(SelectionEvent e) {
+		}
+	}
 
 	class ConfigContentProvider implements ILazyTreeContentProvider {
-		
+
 		private TreeViewer viewer;
+		private ContentSorting sorting;
 		
-		public ConfigContentProvider() {
+		public ConfigContentProvider(ContentSorting s) {
+			sorting = s;
+		}
+
+		public ContentSorting getSorting() {
+			return sorting;
+		}
+		public void setSorting(ContentSorting s) {
+			sorting = s;
 		}
 		
 		@Override
@@ -269,23 +321,36 @@ public class ConfigSelectionDialog extends MessageDialog {
 
 		@Override
 		public Object getParent(Object element) {
+			if (element instanceof IResource)
+				return ((IResource)element).getParent();
 			return null;
 		}
 		
 		// Sort resource members with folders first
 		private List<IResource> getMembers(IFolder folder) {
-			TreeSet<IResource> folders = new TreeSet<IResource>(new Comparator<IResource>() {
+			Comparator<IResource> compareName = new Comparator<IResource>() {
 				@Override
 				public int compare(IResource o1, IResource o2) {
-					return o1.getName().compareTo(o2.getName());
+					return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
 				}
-			});
-			TreeSet<IResource> files = new TreeSet<IResource>(new Comparator<IResource>() {
+			};
+			Comparator<IResource> compareDate = new Comparator<IResource>() {
 				@Override
 				public int compare(IResource o1, IResource o2) {
-					return o1.getName().compareTo(o2.getName());
+					return Long.compare(
+							((IFile)o1).getLocalTimeStamp(),
+							((IFile)o2).getLocalTimeStamp());
 				}
-			});
+			};
+			
+			TreeSet<IResource> folders, files;
+			
+			folders = new TreeSet<IResource>(compareName);
+			if (sorting == ContentSorting.BY_NAME)
+				files = new TreeSet<IResource>(compareName);
+			else
+				files = new TreeSet<IResource>(compareDate);
+			
 			try {
 				for (IResource member : folder.members()) {
 					if (member instanceof IFolder) {
