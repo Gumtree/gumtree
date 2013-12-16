6ba36bff9941103132e1b94f25c9026cf5d5422f
nxi <nxi@NBI0012D.nbi.ansto.gov.au>
2013-12-16 13:40:21 +1100

    Dingo updates

--- bragg/dingo/au.gov.ansto.bragg.dingo.webserver/WebContent/instrumentsetup.js
+++ bragg/dingo/au.gov.ansto.bragg.dingo.webserver/WebContent/instrumentsetup.js
@@ -3,10 +3,10 @@ var devices = [
                {"group":"CAMERA STATUS", 
             	   "items":[{"classId":"plc_secondary", "deviceId":"plc_secondary", "title":"Secondary Shutter", "units":""},
             	            {"classId":"plc_tertiary", "deviceId":"plc_tertiary", "title":"Sample Shutter", "units":""},
-            	            {"classId":"cm1_mode", "deviceId":"cm1_mode", "title":"CM1 Mode", "units":""},  
-            	            {"classId":"cm1_preset", "deviceId":"cm1_preset", "title":"CM1 Preset", "units":""},
-            	            {"classId":"cm1_time", "deviceId":"cm1_time", "title":"CM1 Time", "units":"s"},
-            	            {"classId":"cm1_counts", "deviceId":"cm1_counts", "title":"CM1 Counts", "units":"c"}
+            	            {"classId":"cm1_mode", "deviceId":"cm1_mode", "title":"Camera Mode", "units":""},  
+            	            {"classId":"cm1_preset", "deviceId":"cm1_preset", "title":"Camera Preset", "units":""},
+            	            {"classId":"cm1_time", "deviceId":"cm1_time", "title":"Exposure Time", "units":"s"},
+            	            {"classId":"cm1_counts", "deviceId":"cm1_counts", "title":"Camera Counts", "units":"cts"}
             	            ]
                },
                {"group":"INSTRUMENT CONFIGURATION", 
