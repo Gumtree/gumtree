f24d8a2d685cc913685d4d59d7a5dd1c553c1ab1
nxi <nxi@NBI0012D.nbi.ansto.gov.au>
2014-07-17 13:31:07 +1000

    launch filtered sics model automatically

--- bragg/quokka/au.gov.ansto.bragg.quokka.ui/src/au/gov/ansto/bragg/quokka/ui/QuokkaWorkbenchLauncher.java
+++ bragg/quokka/au.gov.ansto.bragg.quokka.ui/src/au/gov/ansto/bragg/quokka/ui/QuokkaWorkbenchLauncher.java
@@ -11,13 +11,31 @@
 
 package au.gov.ansto.bragg.quokka.ui;
 
+import java.net.URI;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.internal.WorkbenchWindow;
+import org.gumtree.gumnix.sics.control.ISicsController;
+import org.gumtree.gumnix.sics.core.SicsCore;
+import org.gumtree.gumnix.sics.internal.ui.editors.SicsEditorInput;
+import org.gumtree.gumnix.sics.ui.SicsUIConstants;
+import org.gumtree.gumnix.sics.ui.SicsUIProperties;
+import org.gumtree.gumnix.sics.ui.controlview.INodeSet;
+import org.gumtree.gumnix.sics.ui.controlview.NodeSet;
 import org.gumtree.ui.service.launcher.AbstractLauncher;
 import org.gumtree.ui.service.launcher.LauncherException;
 import org.gumtree.ui.service.multimonitor.IMultiMonitorManager;
 import org.gumtree.ui.service.multimonitor.support.MultiMonitorManager;
+import org.gumtree.util.ILoopExitCondition;
+import org.gumtree.util.LoopRunner;
+import org.gumtree.util.string.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,6 +87,62 @@ public class QuokkaWorkbenchLauncher extends AbstractLauncher {
 			}
 //			// position it
 			mmManager.showPerspectiveOnOpenedWindow(ID_PERSPECTIVE_SCRIPTING, 1, 1, mmManager.isMultiMonitorSystem());
+			
+			
+			Thread thread = new Thread(new Runnable(){
+
+				@Override
+				public void run() {
+					LoopRunner.run(new ILoopExitCondition() {
+
+						@Override
+						public boolean getExitCondition() {
+							if (SicsCore.getDefaultProxy() != null && SicsCore.getDefaultProxy().isConnected() && SicsCore.getSicsController() != null){
+								try {
+									Thread.sleep(12000);
+								} catch (InterruptedException e) {
+								}
+								ISicsController sicsController = SicsCore.getSicsController();
+								String filterPath = SicsUIProperties.FILTER_PATH.getValue();
+								if (!StringUtils.isEmpty(filterPath)) {
+									try {
+										IFileStore filterFolder = EFS.getStore(new URI(filterPath));
+										IFileStore child = filterFolder.getChild("CommissioningFilter.xml");
+										INodeSet nodeSet = NodeSet.read(child.openInputStream(EFS.NONE, new NullProgressMonitor()));
+										final SicsEditorInput input = new SicsEditorInput((ISicsController)sicsController, nodeSet);
+										IWorkbenchWindow[] existingWindows = PlatformUI.getWorkbench().getWorkbenchWindows();
+										if (existingWindows.length > 0) {
+											IWorkbenchPage[] pages = existingWindows[existingWindows.length - 1].getPages();
+											if (pages.length > 0) {
+												final IWorkbenchPage page = pages[0];
+												Display.getDefault().asyncExec(new Runnable() {
+
+													@Override
+													public void run() {
+														try {
+															page.openEditor(input, SicsUIConstants.ID_EDITOR_SICS_CONTROL);
+														} catch (PartInitException e) {
+															// TODO Auto-generated catch block
+															e.printStackTrace();
+														}
+													}
+												});
+											}
+										}
+										
+									}catch (Exception e){
+										e.printStackTrace();
+									}
+								}
+								return true;
+							}
+							return false;
+						}
+					}, -1, 1000);
+				}
+
+			});
+			thread.start();
 		}
 	}
 
