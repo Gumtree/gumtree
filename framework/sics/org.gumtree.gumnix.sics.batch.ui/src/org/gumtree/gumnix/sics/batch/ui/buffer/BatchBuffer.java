b89b739641e5b5a9819ea3193a844b02110e250a
nxi <nxi@ansto.gov.au>
2017-06-07 16:16:18 +1000

    Continue with the previous commit

--- framework/sics/org.gumtree.gumnix.sics.batch.ui/src/org/gumtree/gumnix/sics/batch/ui/buffer/BatchBuffer.java
+++ framework/sics/org.gumtree.gumnix.sics.batch.ui/src/org/gumtree/gumnix/sics/batch/ui/buffer/BatchBuffer.java
@@ -24,6 +24,7 @@ public abstract class BatchBuffer extends AbstractModelObject implements IBatchB
 	private int timeEstimation = -1;
 	
 	public BatchBuffer(String name) {
+		name = name.replaceAll(" ", "_");
 		this.name = name;
 	}
 
