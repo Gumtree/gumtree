0572389e70e17a873d52f0db64f5f5dc4192f39a
nxi <nxi@ansto.gov.au>
2018-06-27 10:11:54 +1000

    share org.yaml.snakeyaml

---
+++ bragg/platypus/au.gov.ansto.bragg.platypus.webserver/WebContent/instrumentsetup.js
@@ -0,0 +1,50 @@
+var title = "Platypus";
+var batchEnabled = true;
+var devices = [
+               {"group":"NEUTRON COUNTS", 
+            	   "items":[{"classId":"total_counts", "deviceId":"::histogram_memory::total_counts", "title":"Detector Counts", "units":"ct"}, 
+            	            {"classId":"histogram_memory_time", "deviceId":"::histogram_memory::time", "title":"Time of Counting", "units":"s"}, 
+            	            {"classId":"bm1_counts", "deviceId":"bm1_counts", "title":"BM1 Counts", "units":"ct"},
+            	            {"classId":"bm2_counts", "deviceId":"bm2_counts", "title":"BM2 Counts", "units":"ct"},
+            	            {"classId":"ratemap_xy_total", "deviceId":"::histogram_memory::ratemap_xy_total", "title":"Detector Rate", "units":"ct"}
+            	            ]
+               },
+               {"group":"PREMONOCHROMATOR", 
+            	   "items":[{"classId":"lambda", "deviceId":"lambda", "title":"Wavelength", "units":"\u212B"},  
+            	            {"classId":"momto", "deviceId":"momto", "title":"Take-off angle", "units":"degree"},
+            	            {"classId":"mom", "deviceId":"mom", "title":"Premono Omega", "units":"degree"}
+            	            ]
+               },
+               {"group":"CHOPPERS", 
+            	   "items":[{"classId":"chom", "deviceId":"chom", "title":"Graphite chopper omega", "units":"degree"},  
+            	            {"classId":"chomto", "deviceId":"chomto", "title":"Graphite chopper take-off angle", "units":"degree"},
+            	            {"classId":"gspeed", "deviceId":"/instrument/chpr/graphite/actspeed", "title":"Graphite chopper speed", "units":"rpm"},
+            	            {"classId":"bspeed", "deviceId":"/instrument/chpr/background/actspeed", "title":"Background chopper speed", "units":"rpm"},
+            	            {"classId":"bphase", "deviceId":"/instrument/chpr/background/actphase", "title":"Background chopper phase", "units":"degree"},
+            	            {"classId":"bgear", "deviceId":"/instrument/chpr/background/actgear", "title":"Background chopper ratio", "units":""}
+            	            ]
+               },
+               {"group":"DOPPLER", 
+            	   "items":[{"classId":"damp", "deviceId":"/instrument/doppler/ctrl/amplitude", "title":"Amplitude", "units":"mm"},
+            	            {"classId":"dvel", "deviceId":"/instrument/doppler/ctrl/velocity", "title":"Velocity", "units":"m/s"}
+            	   ]
+               }
+               ];
+
+var nsItems = [
+               {"classId":"reactorPower", "deviceId":"reactorPower", "title":"Reactor Power", "units":"MW"},
+               {"classId":"cnsInTemp", "deviceId":"cnsInTemp", "title":"CNS In Temp", "units":"K"},
+               {"classId":"cnsOutTemp", "deviceId":"cnsOutTemp", "title":"CNS Out Temp", "units":"K"}
+               ];
+
+//var histmemUrl = "dae/rest/image?type=TOTAL_HISTOGRAM_XT&screen_size_x=760&screen_size_y=760";
+
+var histmemUrl = "dae/rest/image?type=$HISTMEM_TYPE";
+
+var histmemTypes = [
+                    {"id" : "TOTAL_HISTOGRAM_T", "text" : "Total t histogram"},
+                    {"id" : "TOTAL_HISTOGRAM_XT", "text" : "Total x-t histogram", "isDefault" : true},
+                    {"id" : "RAW_TOTAL_HISTOGRAM_XTAUX", "text" : "Raw x-taux histogram"},
+                    {"id" : "RAW_TOTAL_HISTOGRAM_YTAUX", "text" : "Raw y-taux histogram"},
+                    {"id" : "RAW_TOTAL_HISTOGRAM_TTAUX", "text" : "Raw t-taux histogram"}                    
+                    ];
---
+++ framework/gumpy/org.gumtree.gumpy.scripts/scripts/gumpy/control/validate.py
@@ -0,0 +1,380 @@
+from org.gumtree.control.core import SicsManager as manager
+from org.gumtree.control.core import ServerStatus
+from org.gumtree.control.events import ISicsControllerListener, ISicsCallback
+from gumpy.commons import logger
+import os
+
+VALIDATOR_PROXY = manager.getValidatorProxy()
+VALIDATOR_MODEL = VALIDATOR_PROXY.getSicsModel()
+
+proxy = VALIDATOR_PROXY
+model = VALIDATOR_MODEL
+
+# if '__IS_VALIDATION_MODE__' in globals():
+#     print 'in globals'
+#     if __IS_VALIDATION_MODE__ :
+#         print 'is validation'
+#         proxy = VALIDATOR_PROXY
+#         model = VALIDATOR_MODEL
+# else:
+#     print 'not in globals'
+
+def get_proxy():
+    return proxy
+
+# class Controller():
+#     
+#     def __init__(self, jcontroller):
+#         self.jcontroller = jcontroller
+#         
+#     def __getattr__(self, attr):
+#         if hasattr(self.jcontroller, attr) :
+#             return getattr(self.jcontroller, attr)
+#         else :
+#             raise AttributeError
+    
+# Get device controller from path or id
+def get_controller(id_or_path):
+#     jcontroller = model.findController(id_or_path)
+#     if not jcontroller is None :
+#         return Controller(jcontroller)
+#     else :
+#         return None
+    c = model.findController(id_or_path)
+    if c is None :
+        raise NameError('controller not found: ' + str(id_or_path))
+    return c
+
+def send_command(command):
+    return proxy.syncRun(command)
+    
+# Asynchronously execute any (adhoc) SICS command (without feedback)
+def execute(command):
+    ret = send_command(command)
+    handle_interrupt()
+    return ret
+
+# Asynchronously set any device or hipadaba node to a given value
+def set_value(name, value):
+    controller = get_controller(name)
+    if (controller == None):
+        raise SicsError('Device / Path ' + name + ' not found')
+    else:
+        controller.setTargetValue(value)
+        controller.commitTargetValue()
+        logger.log('Set ' + name + ' OK')
+
+def hset(parentController, relativePath, value):
+    controller = model.findChildController(parentController, relativePath);
+    controller.setTargetValue(value)
+    controller.commitTargetValue()
+
+def setpos(device, value, real_value):
+    execute('setpos ' + device + ' ' + str(value) + ' ' + str(real_value))
+
+def get_value(name):
+    controller = get_controller(name)
+    if (controller == None):
+        raise SicsError('Device / Path ' + name + ' not found')
+    else:
+        return controller.getValue()
+    
+def get_filename():
+    return get_value('/experiment/file_name')
+    
+# Asynchronously set (run) any device to a given value
+def run(deviceId, value):
+    controller = get_controller(deviceId)
+    controller.setTarget(value)
+    controller.run()
+    handle_interrupt()
+    logger.log("run " + controller.getPath() + " OK")
+
+def pause(on_or_off = True):
+    flag = None
+    if type(on_or_off) is bool :
+        flag = on_or_off
+    elif type(on_or_off) is int :
+        flag = on_or_off > 0
+    elif type(on_or_off) is str :
+        if on_or_off.lower() == 'on' :
+            flag = True
+        elif on_or_off.lower() == 'off' :
+            flag = False
+    if flag == True :
+        status = get_status()
+        if status != ServerStatus.COUNTING and status != ServerStatus.PAUSED :
+            raise SicsError('pause is only available on COUNTING status')
+        else :
+            execute("pause on")
+            logger.log('pause on')
+    elif flag == False:
+        execute("pause off")
+        logger.log('pause off')
+    else :
+        raise SicsError('illegal argument')
+    
+def unpause():
+    pause(False)
+    
+def is_idle():
+    return proxy.getServerStatus().equals(ServerStatus.EAGER_TO_EXECUTE)
+     
+# Synchronously set (drive) any device to a given value
+def drive(deviceId, value):
+    controller = get_controller(deviceId)
+    controller.setTarget(value)
+    controller.drive()
+    handle_interrupt()
+    logger.log("drive " + controller.getPath() + " OK")
+
+# Synchronously drive a number of devices to a given value
+# Usage: multiDrive({'my':-10.0, 'mx':-5.0})
+def multi_drive(entries):
+    proxy.multiDrive(entries)
+    handle_interrupt()
+
+class __ControllerEventHandler__(ISicsControllerListener):
+    
+    def __init__(self):
+        pass
+    
+    def updateState(self, oldState, newState):
+        pass
+        
+    def updateValue(self, oldValue, newValue):
+        pass
+    
+    def updateEnabled(self, isEnabled):
+        pass
+    
+def bmonscan(scan_variable, scan_start, scan_increment, NP, mode, preset):
+    clear_interrupt()
+    controller = get_controller('/commands/scan/bmonscan')
+    p = dict()
+    p['scan_variable'] = scan_variable
+    p['scan_start'] = scan_start
+    p['scan_increment'] = scan_increment
+    p['NP'] = NP
+    p['mode'] = mode
+    p['preset'] = preset
+    __run__(controller, p)
+
+def runhmscan(scan_variable, scan_start, scan_increment, NP, mode, preset):
+    clear_interrupt()
+    controller = get_controller('/commands/scan/hmscan')
+    p = dict()
+    p['scan_variable'] = scan_variable
+    p['scan_start'] = scan_start
+    p['scan_increment'] = scan_increment
+    p['NP'] = NP
+    p['mode'] = mode
+    p['preset'] = preset
+    __run__(controller, p)
+
+def __run__(controller, pars):
+    def log_step(old, new):
+        if float(new) >= 0:
+            logger.log("scan point " + str(new))
+        
+    listener = __ControllerEventHandler__()
+    listener.updateValue = log_step
+    scv = controller.getChild('feedback')
+    if scv :
+        scv = scv.getChild('scan_variable_value')
+    if scv :
+        scv.addControllerListener(listener)
+    # Run scan
+    logger.log('scan started')
+    try :
+        controller.run(pars, None)
+    finally:
+        if scv:
+            scv.removeControllerListener(listener)
+    logger.log('scan completed')
+    handle_interrupt()
+    
+def runscan(scan_variable, scan_start, scan_stop, numpoints, mode, preset, force = 'true', savetype = 'save'):
+    # Initialisation
+    clear_interrupt()
+    controller = get_controller('/commands/scan/runscan')
+    p = dict()
+    p['scan_variable'] = scan_variable
+    p['scan_start'] = scan_start
+    p['scan_stop'] = scan_stop
+    p['numpoints'] = numpoints
+    p['mode'] = mode
+    p['preset'] = preset
+    p['force'] = force
+    p['savetype'] = savetype
+    __run__(controller, p)
+
+
+def count(mode, preset):
+    # Initialisation
+    clear_interrupt()
+    controller = get_controller('/commands/monitor/count')
+    p = dict()
+    p['mode'] = mode
+    p['preset'] = preset
+    __run__(controller, p)
+
+def interrupt():
+    proxy.interrupt()
+    logger.log("Sent SICS interrupt")
+
+def is_interrupted():
+    return proxy.isInterrupted()
+    
+def clear_interrupt():
+    proxy.clearInterruptFlag()
+    
+def handle_interrupt():
+    if is_interrupted():
+        clear_interrupt()
+        raise Exception, 'SICS interrupted!'
+    
+def histmem(cmd, mode, preset):
+    clear_interrupt()
+    controller = get_controller('/commands/histogram/histmem')
+    p = dict()
+    p['cmd'] = cmd
+    p['mode'] = mode
+    p['preset'] = preset
+    __run__(controller, p)
+    
+    
+class SicsError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+    
+__time_out__ = 1
+class __SICS_Callback__(ISicsCallback):
+     
+    def __init__(self, use_full_feedback = False):
+        self.__status__ = None
+        self.__use_full_feedback__ = use_full_feedback
+     
+    def receiveError(self, data):
+        self.__status__ = data.getString()
+        self.setCallbackCompleted(True)
+     
+    def receiveFinish(self, data):
+        self.__status__ = data.getString()
+        self.setCallbackCompleted(True)
+         
+    def receiveReply(self, data):
+        try:
+            rt = data.getString()
+            if self.__use_full_feedback__:
+                status = rt
+            else :
+                if rt.__contains__('='):
+                    status = data.getString().split("=")[1].strip()
+                elif rt.__contains__(':'):
+                    status = data.getString().split(":")[1].strip()
+                    if status.__contains__('}'):
+                        status = status[:status.index('}')]
+                else :
+                    status = rt
+            self.__status__ = status
+            self.setCallbackCompleted(True)
+        except:
+            self.__status__ = data
+            traceback.print_exc(file = sys.stdout)
+            self.setCallbackCompleted(True)
+
+# def run_command(cmd, use_full_feedback = False):
+#     call_back = __SICS_Callback__(use_full_feedback)
+#     proxy.send(cmd, call_back)
+#     return call_back.__status__
+
+def get_raw_value(cmd, dtype = float):
+    res = execute(cmd)
+    if dtype == float :
+        return float(res)
+    else :
+        return res
+
+def get_base_filename():
+    return os.path.basename(str(get_filename()))
+
+def get_status():
+    return proxy.getServerStatus()
+
+def wait_until_idle():
+    if proxy.isConnected() :
+        while not get_status() == ServerStatus.EAGER_TO_EXECUTE:
+            time.sleep(0.5)
+#             cnt += 0.5
+    else:
+        raise Exception, 'disconnected'
+        
+'''
+    Make the system wait until a device reaching a given value.
+    
+    :param device: name or path of the device, e.g., 'samx' or '/sample/samx'.
+    :type device: str
+    
+    :param value: a float number, can't be None
+    :type value: float
+    
+    :param precision: the precision or tolerance of the device. Once the value reaches within the 
+                      tolerance, it finishes waiting. Default value is 0.01.
+    :type precision: float
+    
+    :param timeout_if_not_change: the waiting will finish if the device value doesn't change in 
+                                  this number of seconds. If set to be None, there will be no
+                                  timeout. Default value is None.
+    :type timeout_if_not_change: float value in seconds
+    
+    :param interval: the system will check the device value for every given number of seconds.
+                     It can't be None or 0. Default value is 0.2 seconds.
+    :type interval: float
+    
+    :return: a boolean value if the target value has been reached.
+'''
+def wait_until_value_reached(device, value, precision = 0.01, timeout_if_not_change = None, interval = 0.2):
+    value_reached = False
+    controller = get_controller(device)
+    logger.log('start waiting for ' + str(device) + ' to reach ' + str(value))
+    if precision is None :
+        precision = 0.01
+    if interval is None or interval <= 0 :
+        interval = 0.2
+    if timeout_if_not_change == 0 or timeout_if_not_change is None :
+        timeout_if_not_change = float('nan')
+    old_val = float('nan')
+    update_interval = 5
+    update_count = 0
+    total_count = 0
+    not_change_count = 0
+    while not value_reached:
+        if update_count >= update_interval :
+            try :
+                new_val = controller.getValue()
+                update_count = 0
+            except:
+                new_val = controller.getValue()
+        else:
+            new_val = controller.getValue()
+        if abs(new_val - value) <= precision :
+            value_reached = True
+            break
+        else:
+            if not_change_count > timeout_if_not_change:
+                break
+            if abs(new_val - old_val) <= precision:
+                not_change_count += interval
+            else:
+                not_change_count = 0
+            old_val = new_val
+            update_count += interval
+            total_count += interval
+            time.sleep(interval)
+    if value_reached:
+        logger.log(str(device) + ' reached value ' + str(value) + ' in ' + str(total_count) + ' seconds')
+    else:
+        logger.log(str(device) + ' failed to reach value ' + str(value) + ' in ' + str(total_count) + ' seconds')
