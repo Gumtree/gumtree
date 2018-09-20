3db5b7db1f70fc29e9c573457e82def5cbd93fb0
nxi <nxi@ansto.gov.au>
2018-09-20 16:10:40 +1000

    add header to script

--- bragg/spatz/au.gov.ansto.bragg.spatz.ui/src/au/gov/ansto/bragg/spatz/ui/TclEditorView.java
+++ bragg/spatz/au.gov.ansto.bragg.spatz.ui/src/au/gov/ansto/bragg/spatz/ui/TclEditorView.java
@@ -15,6 +15,10 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.gumtree.gumnix.sics.batch.ui.VisualBatchBufferViewer;
+import org.gumtree.gumnix.sics.batch.ui.commands.ScriptCommand;
+import org.gumtree.gumnix.sics.batch.ui.model.ISicsCommandElement;
+import org.gumtree.gumnix.sics.batch.ui.model.SicsCommandType;
+import org.gumtree.gumnix.sics.batch.ui.util.SicsCommandFactory;
 import org.gumtree.workflow.ui.IWorkflow;
 import org.gumtree.workflow.ui.util.WorkflowFactory;
 
@@ -31,6 +35,10 @@ import au.gov.ansto.bragg.spatz.ui.tasks.PositionTableTask;
 public class TclEditorView extends ViewPart {
 
 	private VisualBatchBufferViewer viewer;
+	private String SICS_SCRIPT_HEADER = "# to select an angle, use 'angler $a', e.g., angler 1\r\n" + 
+			"# to select a position, use 'position $p', e.g., positioner 1\r\n" + 
+			"# to drive a device, use 'drive some_motor some_place', e.g., drive att 10\n";
+	
 	/**
 	 * 
 	 */
@@ -51,7 +59,21 @@ public class TclEditorView extends ViewPart {
 		PositionTableTask positionTableTask = new PositionTableTask();
 		positionTableTask.loadPreference();
 		workflow.addTask(positionTableTask);
-		workflow.addTask(new SicsScriptTask());
+		SicsScriptTask sicsTask = new SicsScriptTask();
+		if (sicsTask.getDataModel() != null) {
+			ISicsCommandElement[] commands = sicsTask.getDataModel().getCommands();
+			if (commands != null && commands.length > 0) {
+				ISicsCommandElement ce = commands[0];
+				if (ce instanceof ScriptCommand) {
+					((ScriptCommand) ce).setText(SICS_SCRIPT_HEADER);
+				}
+			} else {
+				ISicsCommandElement ce = SicsCommandFactory.createCommand(SicsCommandType.SCRIPT);
+				((ScriptCommand) ce).setText(SICS_SCRIPT_HEADER);
+				sicsTask.getDataModel().addCommand(ce);
+			}
+		}
+		workflow.addTask(sicsTask);
 		viewer.setWorkflow(workflow);
 		viewer.afterParametersSet();
 	}
