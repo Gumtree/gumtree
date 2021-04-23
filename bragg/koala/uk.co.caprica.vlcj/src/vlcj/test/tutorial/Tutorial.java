4916aea2d120e67efab67fd5c3dfd78bf91fc7b1
nxi <nxi@ansto.gov.au>
2021-04-23 16:39:46 +1000

    create vlcj libs

---
+++ bragg/koala/uk.co.caprica.vlcj/src/vlcj/test/tutorial/Tutorial.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package vlcj.test.tutorial;
+
+/**
+ * @author nxi
+ *
+ */
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Tutorial {
+
+    private static Tutorial thisApp;
+
+    private final JFrame frame;
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+    
+    private final String filepath = "C:\\Gumtree\\resource";
+    
+    public Tutorial() {
+    	frame = new JFrame("My First Media Player");
+        frame.setBounds(100, 100, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                mediaPlayerComponent.release();
+                System.exit(0);
+            }
+        });
+        
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        frame.setContentPane(mediaPlayerComponent);
+
+        frame.setVisible(true);
+        
+    }
+
+    public void play() {
+    	mediaPlayerComponent.mediaPlayer().media().play(filepath);
+    }
+    
+    public static void main(String[] args) {
+        thisApp = new Tutorial();
+        thisApp.play();
+
+    }
+
+
+}
