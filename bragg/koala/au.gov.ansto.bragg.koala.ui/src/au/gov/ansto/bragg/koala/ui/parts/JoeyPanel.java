8a5c983b1df67eeb8a9d24f943189cde73fd05ce
nxi <nxi@ansto.gov.au>
2021-10-19 11:56:04 +1100

    change pass in joey mode

--- bragg/koala/au.gov.ansto.bragg.koala.ui/src/au/gov/ansto/bragg/koala/ui/parts/JoeyPanel.java
+++ bragg/koala/au.gov.ansto.bragg.koala.ui/src/au/gov/ansto/bragg/koala/ui/parts/JoeyPanel.java
@@ -6,6 +6,7 @@ package au.gov.ansto.bragg.koala.ui.parts;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.ModifyEvent;
@@ -15,6 +16,7 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
@@ -27,14 +29,16 @@ import au.gov.ansto.bragg.koala.ui.Activator;
 public class JoeyPanel extends AbstractPanel {
 
 	private static final String UNLOCK_TEXT = "koala123";
-	private static final int WIDTH_HINT = 480;
+	private static final int WIDTH_HINT = 600;
 	private static final int HEIGHT_HINT = 680;
 	private MainPart mainPart;
 	private Button activeButton;
 	private Button advButton;
 	private Label statusLabel;
 	private boolean isActivated;
-	
+	private ScrolledComposite advHolder;
+	private Group passPart;
+
 	/**
 	 * @param parent
 	 * @param style
@@ -43,7 +47,7 @@ public class JoeyPanel extends AbstractPanel {
 		super(parent, style);
 		mainPart = part;
 		GridLayoutFactory.fillDefaults().numColumns(2).margins(20, 8).applyTo(this);
-		GridDataFactory.swtDefaults().minSize(480, 320).align(SWT.CENTER, SWT.CENTER).applyTo(this);
+		GridDataFactory.swtDefaults().minSize(480, 480).align(SWT.CENTER, SWT.CENTER).applyTo(this);
 		
 		Label passLabel = new Label(this, SWT.NONE);
 		passLabel.setText("Password");
@@ -51,7 +55,7 @@ public class JoeyPanel extends AbstractPanel {
 		GridDataFactory.fillDefaults().grab(false, false).align(SWT.CENTER, SWT.CENTER).applyTo(passLabel);
 		final Text passText = new Text(this, SWT.PASSWORD | SWT.BORDER);
 		passText.setFont(Activator.getMiddleFont());
-		GridDataFactory.fillDefaults().grab(true, true).minSize(240, 36).align(SWT.END, SWT.CENTER).applyTo(passText);
+		GridDataFactory.fillDefaults().grab(true, true).minSize(320, 36).align(SWT.END, SWT.CENTER).applyTo(passText);
 
 		passText.addKeyListener(new KeyListener() {
 			
@@ -64,6 +68,8 @@ public class JoeyPanel extends AbstractPanel {
 				} else {
 					activeButton.setEnabled(false);
 					advButton.setVisible(false);
+					advButton.setSelection(false);
+					hideAdvPanel();
 				}
 			}
 			
@@ -89,8 +95,61 @@ public class JoeyPanel extends AbstractPanel {
 		advButton.setVisible(false);
 		advButton.setToolTipText("Click here to change password");
 		advButton.setFont(Activator.getMiddleFont());
-		GridDataFactory.fillDefaults().grab(true, false).minSize(240, 36).align(SWT.END, SWT.BEGINNING).applyTo(advButton);
+		GridDataFactory.fillDefaults().grab(true, false).minSize(320, 36).align(SWT.END, SWT.BEGINNING).applyTo(advButton);
 
+		advHolder = new ScrolledComposite(this, SWT.NONE);
+		GridLayoutFactory.fillDefaults().margins(4, 4).applyTo(advHolder);
+		GridDataFactory.fillDefaults().grab(true, false).minSize(400, 180).span(2, 1).applyTo(advHolder);
+		advHolder.setExpandHorizontal(true);
+		advHolder.setExpandVertical(true);
+		
+		passPart = new Group(advHolder, SWT.BORDER);
+		passPart.setBackground(Activator.getLightColor());
+		passPart.setText("Change password");
+		GridLayoutFactory.fillDefaults().margins(8, 12).numColumns(2).applyTo(passPart);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(passPart);
+		
+		final Label newLabel = new Label(passPart, SWT.NONE);
+		newLabel.setText("New password");
+		newLabel.setFont(Activator.getMiddleFont());
+		GridDataFactory.fillDefaults().grab(false, false).align(SWT.CENTER, SWT.CENTER).applyTo(newLabel);
+		final Text newText = new Text(passPart, SWT.PASSWORD | SWT.BORDER);
+		newText.setFont(Activator.getMiddleFont());
+		GridDataFactory.fillDefaults().grab(true, true).minSize(240, 36).align(SWT.END, SWT.CENTER).applyTo(newText);
+
+		final Label reLabel = new Label(passPart, SWT.NONE);
+		reLabel.setText("Repeat new password");
+		reLabel.setFont(Activator.getMiddleFont());
+		GridDataFactory.fillDefaults().grab(false, false).align(SWT.CENTER, SWT.CENTER).applyTo(reLabel);
+		final Text reText = new Text(passPart, SWT.PASSWORD | SWT.BORDER);
+		reText.setFont(Activator.getMiddleFont());
+		GridDataFactory.fillDefaults().grab(true, true).minSize(240, 36).align(SWT.END, SWT.CENTER).applyTo(reText);
+
+		Button applyButton = new Button(passPart, SWT.PUSH);
+		applyButton.setCursor(Activator.getHandCursor());
+		applyButton.setText("Apply new password");
+		applyButton.setEnabled(false);
+		applyButton.setFont(Activator.getMiddleFont());
+		GridDataFactory.fillDefaults().grab(true, true).minSize(320, 36).align(SWT.CENTER, SWT.CENTER).span(2, 1).applyTo(applyButton);
+
+		advButton.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (advButton.getSelection()) {
+					advHolder.setContent(passPart);
+					passPart.layout();
+					advHolder.setMinSize(passPart.computeSize(400, 80));
+					layout();
+				} else {
+					advHolder.setContent(null);
+				}
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
 		
 		statusLabel = new Label(this, SWT.NONE);
 		statusLabel.setText("Inactive");
@@ -112,6 +171,8 @@ public class JoeyPanel extends AbstractPanel {
 			public void widgetSelected(SelectionEvent e) {
 				isActivated = !isActivated;
 				passText.setText("");
+				advButton.setVisible(false);
+				hideAdvPanel();
 				if (isActivated) {
 					statusLabel.setText("ACTIVE");
 					statusLabel.setEnabled(true);
@@ -126,7 +187,6 @@ public class JoeyPanel extends AbstractPanel {
 				mainPart.setJoeyMode(isActivated);
 				passText.setFocus();
 				activeButton.setEnabled(false);
-				activeButton.setGrayed(true);
 			}
 			
 			@Override
@@ -135,6 +195,10 @@ public class JoeyPanel extends AbstractPanel {
 		});
 	}
 
+	private void hideAdvPanel() {
+		advHolder.setContent(null);
+		advHolder.layout();
+	}
 	
 	/* (non-Javadoc)
 	 * @see au.gov.ansto.bragg.koala.ui.parts.AbstractControlPart#next()
