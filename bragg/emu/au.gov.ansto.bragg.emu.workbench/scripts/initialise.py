e7ae70ac246c7ab6b9eb0084eb63f01e7ebba8f6
nxi <nxi@ansto.gov.au>
2023-08-28 15:22:47 +1000

    add init script for Emu

---
+++ bragg/emu/au.gov.ansto.bragg.emu.workbench/scripts/initialise.py
@@ -0,0 +1,22 @@
+# Initial import
+
+###############################################################################
+#
+# Import sics
+#
+###############################################################################
+
+from gumpy.commons import sics
+
+###############################################################################
+#
+# Quokka related import
+#
+###############################################################################
+
+# Import logger
+from gumpy.commons.logger import log
+
+# Import all
+# from bragg.bilby import bilby
+# from bragg.bilby.bilby import *
