cce95a7c49f6098541357eee2b853f4ded12b83c
nxi <nxi@ansto.gov.au>
2023-10-27 16:19:00 +1100

    Add scrollable window to koala

--- bragg/koala/au.gov.ansto.bragg.koala.ui/src/au/gov/ansto/bragg/koala/ui/parts/KoalaMainView.java
+++ bragg/koala/au.gov.ansto.bragg.koala.ui/src/au/gov/ansto/bragg/koala/ui/parts/KoalaMainView.java
@@ -2,12 +2,16 @@ package au.gov.ansto.bragg.koala.ui.parts;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 
 public class KoalaMainView extends ViewPart {
 
+	private static final int SCROLL_WIDTH = 2200;
+	private static final int SCROLL_HEIGHT = 1280;
+	
 	private KoalaMainViewer viewer;
 	
 	public KoalaMainView() {
@@ -15,8 +19,15 @@ public class KoalaMainView extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		viewer = new KoalaMainViewer(parent, SWT.NONE);
+		ScrolledComposite container = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
+		viewer = new KoalaMainViewer(container, SWT.NONE);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(viewer);
+		container.setContent(viewer);
+		container.setMinSize(SCROLL_WIDTH, SCROLL_HEIGHT);
+
+	    // Expand both horizontally and vertically
+		container.setExpandHorizontal(true);
+		container.setExpandVertical(true);
 	}
 
 	@Override
