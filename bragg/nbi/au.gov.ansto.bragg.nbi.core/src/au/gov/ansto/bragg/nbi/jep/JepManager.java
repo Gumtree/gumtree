a9f1c5ac008e50b7268a73e74055d2f165d9f52a
nxi <nxi@ansto.gov.au>
2024-08-08 11:29:47 +1000

    add jep manager

---
+++ bragg/nbi/au.gov.ansto.bragg.nbi.core/src/au/gov/ansto/bragg/nbi/jep/JepManager.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package au.gov.ansto.bragg.nbi.jep;
+
+/**
+ * @author nxi
+ *
+ */
+public class JepManager {
+
+	/**
+	 * 
+	 */
+	public JepManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
