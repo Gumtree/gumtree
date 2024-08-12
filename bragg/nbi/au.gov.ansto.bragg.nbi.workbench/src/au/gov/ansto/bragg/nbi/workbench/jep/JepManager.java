0a38b92e22d2535862b830c0558bf90eac71a151
nxi <nxi@ansto.gov.au>
2024-08-12 14:39:21 +1000

    add jep to build

--- bragg/nbi/au.gov.ansto.bragg.nbi.workbench/src/au/gov/ansto/bragg/nbi/workbench/jep/JepManager.java
+++ bragg/nbi/au.gov.ansto.bragg.nbi.workbench/src/au/gov/ansto/bragg/nbi/workbench/jep/JepManager.java
@@ -3,6 +3,14 @@
  */
 package au.gov.ansto.bragg.nbi.workbench.jep;
 
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import jep.Interpreter;
+import jep.JepConfig;
+import jep.SubInterpreter;
+
 /**
  * @author nxi
  *
@@ -12,8 +20,28 @@ public class JepManager {
 	/**
 	 * 
 	 */
+	private static Map<OutputStream, Interpreter> interpMap = new HashMap<OutputStream, Interpreter>();
+	
+	private OutputStream writer;
+	
 	public JepManager() {
-		// TODO Auto-generated constructor stub
 	}
 
+	private static Interpreter createInterpreter(OutputStream writer) {
+		JepConfig config = new JepConfig();
+		config.redirectStdErr(writer);
+		config.redirectStdout(writer);
+		Interpreter interp = new SubInterpreter(config);
+		interpMap.put(writer, interp);
+		return interp;
+	}
+	
+	public synchronized static Interpreter getInterpreter(OutputStream writer) {
+		if (interpMap.containsKey(writer)) {
+			return interpMap.get(writer);
+		} else {
+			return createInterpreter(writer);
+		}
+	}
+	
 }
