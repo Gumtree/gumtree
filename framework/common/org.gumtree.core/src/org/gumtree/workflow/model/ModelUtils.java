a5493fce2884247387d3ac37757e83c6c14ebdde
Norman Xiong <norman.xiong@ansto.gov.au>
2026-08-07 15:42:42 +1000

    fix gson version conflicts

--- framework/common/org.gumtree.core/src/org/gumtree/workflow/model/ModelUtils.java
+++ framework/common/org.gumtree.core/src/org/gumtree/workflow/model/ModelUtils.java
@@ -2,16 +2,16 @@ package org.gumtree.workflow.model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.graph.GraphAdapterBuilder;
+//import com.google.gson.graph.GraphAdapterBuilder;
 
 public final class ModelUtils {
 
 	static {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		// Graph adapter
-		new GraphAdapterBuilder().addType(ElementModel.class)
-				.addType(WorkflowModel.class).addType(TaskModel.class)
-				.registerOn(gsonBuilder);
+//		new GraphAdapterBuilder().addType(ElementModel.class)
+//				.addType(WorkflowModel.class).addType(TaskModel.class)
+//				.registerOn(gsonBuilder);
 		// Create gson
 		gson = gsonBuilder.setPrettyPrinting().create();
 	}
